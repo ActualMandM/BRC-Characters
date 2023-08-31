111931c2281cdb53f0b2c4259f2013f15dae4150
Michael <15317421+ActualMandM@users.noreply.github.com>
2023-08-30 20:52:48 -0700

    [Candyman] Add outfit names, fix version number

--- BRC-Candyman/Plugin.cs
+++ BRC-Candyman/Plugin.cs
@@ -14,7 +14,7 @@ namespace BRC_Candyman
 	{
 		public const string ModGuid = "com.MandM.BRC-Candyman";
 		public const string ModName = "BRC-Candyman";
-		public const string ModVer = "1.0.0";
+		public const string ModVer = "1.1.0";
 
 		public void Awake()
 		{
@@ -26,13 +26,13 @@ namespace BRC_Candyman
 			candy.defaultOutfit = 0;
 			candy.defaultMoveStyle = MoveStyle.SKATEBOARD;
 			candy.tempAudioBase = Characters.eightBallBoss;
-			candy.freestyleType = ModdedCharacterConstructor.FreestyleType.freestyle16;
-			candy.bounceType = ModdedCharacterConstructor.BounceType.softbounce3;
+			candy.freestyleType = ModdedCharacterConstructor.FreestyleType.freestyle12;
+			candy.bounceType = ModdedCharacterConstructor.BounceType.softbounce1;
 
-			candy.AddOutfit(bundle.LoadAsset<Material>("candyMat1.mat"));
-			candy.AddOutfit(bundle.LoadAsset<Material>("candyMat2.mat"));
-			candy.AddOutfit(bundle.LoadAsset<Material>("candyMat3.mat"));
-			candy.AddOutfit(bundle.LoadAsset<Material>("candyMat4.mat"));
+			candy.AddOutfit(bundle.LoadAsset<Material>("candyMat1.mat"), "Standard");
+			candy.AddOutfit(bundle.LoadAsset<Material>("candyMat2.mat"), "Casino");
+			candy.AddOutfit(bundle.LoadAsset<Material>("candyMat3.mat"), "Pastel");
+			candy.AddOutfit(bundle.LoadAsset<Material>("candyMat4.mat"), "Saturate");
 
 			//candy.AddPersonalGraffiti("Candyman", "N/A", bundle.LoadAsset<Material>("candyGraffiti.mat"), bundle.LoadAsset<Texture>("graffitiTexture.png"));
 			candy.personalGraffitiBase = Characters.eightBall;

24d58aa18e65bda6ffd5259490dbe12947617d1e
Michael <15317421+ActualMandM@users.noreply.github.com>
2023-09-15 22:10:36 -0700

    [Candyman] Update to v1.2.1

--- BRC-Candyman/Plugin.cs
+++ BRC-Candyman/Plugin.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 
 namespace BRC_Candyman
 {
-	[BepInPlugin("com.MandM.BRC-Candyman", "BRC-Candyman", "1.2.0")]
+	[BepInPlugin("com.MandM.BRC-Candyman", "BRC-Candyman", "1.2.1")]
 	[BepInDependency("com.Viliger.CharacterAPI", "0.6.0")]
 
 	public class Plugin : BaseUnityPlugin
@@ -19,15 +19,15 @@ namespace BRC_Candyman
 			chara.characterName = "Candyman";
 			chara.characterPrefab = bundle.LoadAsset<GameObject>("candy_character");
 			chara.defaultOutfit = 0;
-			chara.defaultMoveStyle = MoveStyle.SKATEBOARD;
+			chara.defaultMoveStyle = MoveStyle.BMX;
 			chara.audioClips = bundle.LoadAllAssets<AudioClip>().ToList();
 			chara.freestyleType = ModdedCharacterConstructor.FreestyleType.freestyle12;
 			chara.bounceType = ModdedCharacterConstructor.BounceType.softbounce1;
 
-			chara.AddOutfit(bundle.LoadAsset<Material>("candyMat1.mat"), "Standard");
-			chara.AddOutfit(bundle.LoadAsset<Material>("candyMat2.mat"), "Casino");
-			chara.AddOutfit(bundle.LoadAsset<Material>("candyMat3.mat"), "Pastel");
-			chara.AddOutfit(bundle.LoadAsset<Material>("candyMat4.mat"), "Saturate");
+			chara.AddOutfit(bundle.LoadAsset<Material>("candyMat0.mat"), "Standard");
+			chara.AddOutfit(bundle.LoadAsset<Material>("candyMat1.mat"), "Casino");
+			chara.AddOutfit(bundle.LoadAsset<Material>("candyMat2.mat"), "Pastel");
+			chara.AddOutfit(bundle.LoadAsset<Material>("candyMat3.mat"), "Saturate");
 
 			chara.AddPersonalGraffiti("Candyman", "Team Reptile", bundle.LoadAsset<Material>("candyGraffiti.mat"), bundle.LoadAsset<Texture>("graffitiTexture.png"));
 			chara.CreateModdedCharacter();
